c8c1016718d8d6aecfd1e75b729d926298f7f5a9
noahbei <nbeidelman9@gmail.com>
2024-12-04 22:45:50 -0800

    update to track stand or hit

--- search.py
+++ search.py
@@ -11,51 +11,52 @@ def simulate_outcome(deck, hand, dealer_hand, depth=3):
     - depth: Number of levels to explore in the decision tree
     Returns a dictionary of outcomes (win, lose, tie, bust).
     """
-    outcomes = {"win": 0, "lose": 0, "tie": 0, "bust": 0}
-    queue = deque([(hand, dealer_hand, deck, True, 0)])  # (player_hand, dealer_hand, deck, is_player_turn, current_depth)
+    outcomes = {"win": {"stand": 0, "hit": 0, "None": 0}, "lose": {"stand": 0, "hit": 0, "None": 0},
+                "tie": {"stand": 0, "hit": 0, "None": 0}, "bust": {"stand": 0, "hit": 0, "None": 0}}
+    queue = deque([(hand, dealer_hand, deck, True, 0, "None")])  # (player_hand, dealer_hand, deck, is_player_turn, current_depth, hit_or_stand)
 
     while queue:
-        curr_hand, curr_dealer_hand, curr_deck, is_player_turn, curr_depth = queue.popleft()
+        curr_hand, curr_dealer_hand, curr_deck, is_player_turn, curr_depth, action = queue.popleft()
 
         # Terminal state or maximum depth reached
         if curr_depth >= depth or calculate_hand_value(curr_hand) > 21:
             player_value = calculate_hand_value(curr_hand)
             dealer_value = calculate_hand_value(curr_dealer_hand)
-
+            
             if player_value > 21:
-                outcomes["lose"] += 1
+                outcomes["lose"][action] += 1
             elif dealer_value > 21:
-                outcomes["win"] += 1
+                outcomes["win"][action] += 1
             elif player_value > dealer_value:
-                outcomes["win"] += 1
+                outcomes["win"][action] += 1
             elif player_value < dealer_value:
-                outcomes["lose"] += 1
+                outcomes["lose"][action] += 1
             else:
-                outcomes["tie"] += 1
+                outcomes["tie"][action] += 1
             continue
 
         # Player's turn
         if is_player_turn:
             # "Stand" branch
-            queue.append((curr_hand, curr_dealer_hand, curr_deck, False, curr_depth + 1))
+            queue.append((curr_hand, curr_dealer_hand, curr_deck, False, curr_depth + 1, "stand"))
 
             # "Hit" branch
             for i, card in enumerate(curr_deck):
                 new_hand = curr_hand + [card]
                 new_deck = curr_deck[:i] + curr_deck[i + 1:]
-                queue.append((new_hand, curr_dealer_hand, new_deck, True, curr_depth + 1))
+                queue.append((new_hand, curr_dealer_hand, new_deck, True, curr_depth + 1, "hit"))
 
         # Dealer's turn
         else:
             dealer_value = calculate_hand_value(curr_dealer_hand)
             if dealer_value >= 17:
                 # Evaluate final state
-                queue.append((curr_hand, curr_dealer_hand, curr_deck, False, curr_depth + 1))
+                queue.append((curr_hand, curr_dealer_hand, curr_deck, False, curr_depth + 1, "None"))
             else:
                 for i, card in enumerate(curr_deck):
                     new_dealer_hand = curr_dealer_hand + [card]
                     new_deck = curr_deck[:i] + curr_deck[i + 1:]
-                    queue.append((curr_hand, new_dealer_hand, new_deck, False, curr_depth + 1))
+                    queue.append((curr_hand, new_dealer_hand, new_deck, False, curr_depth + 1, "None"))
 
     return outcomes
 
@@ -66,8 +67,8 @@ def recommend_action(player_hand, dealer_hand, deck, depth=3):
     outcomes_hit = simulate_outcome(deck, player_hand, dealer_hand, depth)
 
     # Calculate win probabilities
-    prob_hit = outcomes_hit["win"] / (sum(outcomes_hit.values()) or 1)
-    prob_stand = outcomes_hit["lose"] / (sum(outcomes_hit.values()) or 1)
+    prob_hit = outcomes_hit["win"]["hit"] / (sum(outcomes_hit["win"].values()) or 1)
+    prob_stand = outcomes_hit["win"]["stand"] / (sum(outcomes_hit["win"].values()) or 1)
 
     return "hit" if prob_hit > prob_stand else "stand"
 
